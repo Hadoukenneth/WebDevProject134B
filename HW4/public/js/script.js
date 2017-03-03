0354730022cf644d6b618ee9b730dde6687b5d88
Lucky <luckyknguyen@gmail.com>
2017-03-02 19:56:41 -0800

    testing auth

--- HW4/public/js/script.js
+++ HW4/public/js/script.js
@@ -30,17 +30,16 @@
 
     firebase.auth().signInWithEmailAndPassword(email, password)
     .catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  if (errorCode === 'auth/wrong-password') {
-    alert('Wrong password.');
-  } else {
-    alert(errorMessage);
-  }
-  console.log(error);
-});
-
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
   });
 
   btnSignup.addEventListener('click', function(){
