097f45ecdcdc20699d8e78d88b2771f941e8e729
Lucky <luckyknguyen@gmail.com>
2017-03-06 01:38:59 -0800

    create watchlist

--- HW4/public/js/add.js
+++ HW4/public/js/add.js
@@ -1,5 +1,4 @@
 (function(){
-  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCuaRPCoQegFXgMeIXR2ZSaXaDWu3KukPo",
     authDomain: "moviedex-99d1a.firebaseapp.com",
@@ -36,4 +35,28 @@
         }
       }
     });
+
+    var btnLogout = document.getElementById('signout');
+
+    btnLogout.addEventListener('click', function(){
+      // firebase.auth().signOut();
+      firebase.auth().signOut().then(function() {
+        console.log('log out');
+        window.location = '/';
+      }, function(error) {
+        console.log('error');
+      });
+    });
+
+
+
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('signedin');
+    } else {
+      console.log('not signedin');
+    }
+  });
+
+
 }());
