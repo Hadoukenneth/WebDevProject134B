0fc4ae8526d97d79a0d4f825535cbe79408a5976
Kenneth Vu <knnthvu@yahoo.com>
2017-03-06 22:29:48 -0800

    Moved script tags to the bottom of the body to up the speed. Moved javascript in profile.html to its own js folder

---
+++ HW4/public/js/profile.js
@@ -0,0 +1,13 @@
+(function changeTabs(evt, tabName) {
+	var i, x, tablinks;
+	x = document.getElementsByClassName("tabs");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < x.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace("active", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}());
